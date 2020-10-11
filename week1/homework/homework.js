cb06f2348a1b838530321d15abccf22a0767064a
sophryu99 <sehwa_ryu@berkeley.edu>
2020-10-11 23:36:52 +0900

    Add Week1 homework

---
+++ week1/homework/homework.js
@@ -0,0 +1,21 @@
+// Level 1: git commit
+
+// Level 2: 자신의 팀원들을 소개할 수 있는 json Array 만들기 (팀원들의 이름, 사는곳, 나이, 취미, 정보를 출력하는 함수를 포함!)
+const members = [
+    {
+        name: "류세화",
+        home: "교대",
+        age: "23",
+        hobby: "youtube"
+    },
+    {
+        name: "정효원",
+        home: "성신여대",
+        age: "24",
+        hobby: "sleep"
+    }
+
+]
+
+// Level 3: 가짜 서버파트 members.js 데이터를 이용해서 랜덤으로 조를 짜는 알고리즘 만들어보기.
+
