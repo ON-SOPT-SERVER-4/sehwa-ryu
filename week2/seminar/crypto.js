21df9866e7445f4f1fba03073250f3a5437f278a
sophryu99 <sehwa_ryu@berkeley.edu>
2020-10-28 20:22:08 +0900

    docs

--- week2/seminar/crypto.js
+++ week2/seminar/crypto.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const password = '123123';
 const password2 = '12312';
 const base64 = crypto.createHash('sha512').update(password).digest('base64');
-const base64 = crypto.createHash('sha512').update(password2).digest('base64');
+const base64_ = crypto.createHash('sha512').update(password2).digest('base64');
 const hex = crypto.createHash('sha512').update(password).digest('hex');
 
 console.log(base64);
@@ -16,5 +16,4 @@ crypto.randomBytes(64, (err, buf) => {
     crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, key) =>{
       console.log(`password: ${key.toString('base64')}`);
     });
-  });
-  
+});
