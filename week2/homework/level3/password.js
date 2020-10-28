9f891f4e2656a560e56d49e5d1dcfdee2cc626a3
sophryu99 <sehwa_ryu@berkeley.edu>
2020-10-28 19:23:41 +0900

    Week2 Level3: done

---
+++ week2/homework/level3/password.js
@@ -0,0 +1,17 @@
+// LEVEL 3
+// fs 모듈을 이용하여 비동기적으로 비밀번호 암호화하기
+
+const fs = require('fs');
+const crypto = require('crypto');
+
+const fileName = 'password'
+const password = 'sopt27thServer';
+
+crypto.randomBytes(64, (err, buf) => {
+    const salt = buf.toString('base64');
+    crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, key) =>{
+      fs.writeFile(`${fileName}.txt`,key.toString('base64'),()=>{
+        console.log(`file ${fileName} created!`);
+    })
+    });
+});
