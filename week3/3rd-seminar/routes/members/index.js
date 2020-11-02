10cf2bd0101ff578d90067ab1e83f7cf98dfbe61
sophryu99 <sehwa_ryu@berkeley.edu>
2020-11-03 01:03:46 +0900

    Week3 Level2: done

--- week3/3rd-seminar/routes/members/index.js
+++ week3/3rd-seminar/routes/members/index.js
@@ -14,8 +14,6 @@ router.post('/', (req, res) => {
         console.log('필요한 값이 없습니다');
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
     }
-
-
     const idx = membersDB[membersDB.length - 1].idx + 1;
     membersDB.push({
         idx,
@@ -24,7 +22,6 @@ router.post('/', (req, res) => {
         age
     })
     return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.MEMBER_CREATE_SUCCESS, membersDB));
-
 });
 
 /** 모든 멤버 조회 */
