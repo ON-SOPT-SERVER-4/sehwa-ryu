4c0ee1ba908e18aab9242545f6cbde604c0d54cc
sophryu99 <sehwa_ryu@berkeley.edu>
2020-11-03 00:59:52 +0900

    Add gitignore

--- week3/3rd-seminar/routes/members/index.js
+++ week3/3rd-seminar/routes/members/index.js
@@ -11,9 +11,10 @@ router.post('/', (req, res) => {
     const { name, part, age } = req.body;
 
     if (!name || !part || !age) {
-        console.log('필요한 값이 없습니다'); 
+        console.log('필요한 값이 없습니다');
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
     }
+
 
     const idx = membersDB[membersDB.length - 1].idx + 1;
     membersDB.push({
@@ -35,8 +36,7 @@ router.get('/', (req, res) => {
 /** idx 값으로 특정 멤버 조회 */
 router.get('/:idx', (req, res) => {
     const { idx } = req.params;
-
-    if ( !idx ) {
+    if (!idx) {
         console.log('필요한 값이 없습니다!');
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
     }
@@ -53,7 +53,7 @@ router.get('/:idx', (req, res) => {
 /** idx값으로 특정 멤버 삭제 */
 router.delete('/:idx', (req, res) => {
     const { idx } = req.params;
-    if ( !idx ) {
+    if (!idx) {
         console.log('필요한 값이 없습니다!');
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
     }
@@ -68,6 +68,31 @@ router.delete('/:idx', (req, res) => {
 
 /** idx값으로 특정 멤버 정보 수정 */
 router.put('/:idx', (req, res) => {
+    const { idx } = req.params;
+    if (!idx) {
+        console.log('필요한 값이 없습니다!');
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+    if (!name || !part || !age) {
+        console.log('필요한 값이 없습니다!');
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+
+    const memberIdx = membersDB.findIndex(member => member.idx == idx);
+
+    if (memberIdx === -1) {
+        console.log('idx가 유효하지 않습니다.');
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+    }
+
+    membersDB[memberIdx] = {
+        idx: Number.parseInt(idx),
+        name,
+        part,
+        age,
+    }
+
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.MEMBER_UPDATE_SUCCESS, membersDB));
 
 });
 
---
+++ week3/3rd-seminar/routes/users/index.js
@@ -0,0 +1,46 @@
+const crypto = require('crypto');
+const express = require('express');
+const router = express.Router();
+const util = require('../../modules/util');
+const responseMessage = require('../../modules/responseMessage');
+const statusCode = require('../../modules/statusCode');
+let usersDB = require('../../modules/users');
+const { response } = require('express');
+
+router.post('/signup', (req, res) => {
+  const {id, password} = req.body; // 1. req.body에서 데이터 가져오기
+
+  //2. request data 확인하기, id 또는 password data가 없다면 NullValue 반환
+  if ( !id || !password) {
+      console.log("필요한 값이 없습니다!");
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE))
+  }
+
+  //3. 존재하는 아이디인지 확인하기. 이미 존재하는 아이디면 ALREADY ID 반환
+  const idCheck = usersDB.find(user => user.id == id);
+  if (idCheck) {
+      console.log("이미 존재하는 아이디입니다!");
+      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.ALREADY_ID))
+  }
+
+  //4. salt 생성
+  const salt = crypto.randomBytes(64).toString('base64');
+
+  //5. 2차 세미나때 배웠던 pbkdf2 방식으로 (비밀번호 + salt) 해싱하여 => 암호화된 password 를 만들기!
+  //6. usersDB에 id, 암호화된 password, salt 저장!
+  //7. status: 200 message: SING_UP_SUCCESS, data: id만 반환! (비밀번호, salt 반환 금지!!)
+})
+
+router.post('/signin', (req, res) => {
+  const {id, password} = req.body; // 1. req.body에서 데이터 가져오기
+  //2. request data 확인하기, id 또는 password data가 없다면 NullValue 반환
+  //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
+  //4. 비밀번호 확인하기 - 로그인할 id의 salt를 DB에서 가져와서  사용자가 request로 보낸 password와
+  //   암호화를 한후 디비에 저장되어있는 password와 일치하면 true일치하지 않으면 Miss Match password 반환
+  //5. status: 200 ,message: SIGNIN SUCCESS, data: id 반환 (비밀번호, salt 반환 금지!!)
+})
+router.get('/', (req, res) => {
+  // 1.모든 유저정보 조회 (id, password, salt)!
+})
+
+module.exports = router;
